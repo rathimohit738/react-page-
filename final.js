76bbbbc10c13f499237237eef2b3e5d971c281a9
rathimohit738 <109451173+rathimohit738@users.noreply.github.com>
2022-10-22 15:34:36 +0530

    Create final.js

---
+++ final.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Final = () => {
+    const mystyle = {
+        color: "white",
+        backgroundColor: "LightGray",
+        padding: "10px",
+        fontFamily: "Arial"
+         };
+  return (
+<div style={mystyle}> 
+    <div>
+        <h3 style={{color:"black",bottom:"-70px",position:"relative",color: "red"}}>
+                          Copyright 2022 Inc. All Rights Reserved. ,</h3>
+       <h3 style={{color:"black",bottom:"-70px",position:"relative",color: "orange"}}>User Agreement, Privacy policey </h3> 
+
+</div>
+    </div>
+  )
+}
+
+export default Final

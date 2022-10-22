9e79078f86eb11356555115b71dec9b9f04f662e
rathimohit738 <109451173+rathimohit738@users.noreply.github.com>
2022-10-22 15:33:55 +0530

    Create second.js

---
+++ second.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Second = () => {
+   
+  return (
+    <div style={{top:"30px",position:"relative",color: "red"}}>
+        <img width="750px" height="450px" src="https://media.istockphoto.com/photos/diwali-oil-lamp-picture-id803533956?k=20&m=803533956&s=612x612&w=0&h=CErnq1JTpKTHG6ZJR3-HMEj8LRe5KZfA4SJ82iEiGYo="/>
+        <img width="600px" height="450px" src="https://thumbs.dreamstime.com/b/diwali-oil-lamp-diya-lit-colorful-rangoli-75658925.jpg"/>
+    
+    </div>
+  )
+}
+
+export default Second
